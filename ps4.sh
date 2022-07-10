9fcd2a02f3cbef7a132bcc2aaad8fbb511e472ce
Tristan Kennedy <tristan@taskinoz.com>
2022-07-10 23:13:24 +1000

    Added build script and ignore files

---
+++ ps4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## This is an auto build script that can be used with the new RSPNVPK tool
+## https://github.com/mrsteyk/RSPNVPK
+## Add the VPK that your editing in a /dir folder
+## eg. englishclient_frontend.bsp.pak000_dir.vpk
+
+rm ./*.vpk
+
+## Run on Dev
+cp ./ps4dir/*.vpk ./
+wait
+RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -d ./src -s
