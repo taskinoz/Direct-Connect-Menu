a7e4f8bf1c191e69ac33629efe838762a9d37023
Tristan Kennedy <tristan@taskinoz.com>
2023-01-22 20:09:14 +1100

    Updated build script to move it to plugin folder

--- ps4.sh
+++ ps4.sh
@@ -5,9 +5,11 @@
 ## Add the VPK that your editing in a /dir folder
 ## eg. englishclient_frontend.bsp.pak000_dir.vpk
 
-rm ./*.vpk
+# rm ./*.vpk
 
 ## Run on Dev
 cp ./ps4dir/*.vpk ./
 wait
 RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -d ./src -s
+
+mv ./*.vpk ../titanfall-plugin/CUSA04013/vpk_ps4
