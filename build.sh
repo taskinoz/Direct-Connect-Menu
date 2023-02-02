123e787a849fb675705b07caaa192b22678b633c
Tristan Kennedy <tristan@taskinoz.com>
2023-02-02 23:09:17 +1100

    Fixed error using wrong vpks

--- build.sh
+++ build.sh
@@ -12,34 +12,26 @@ then
     ## Run on Prod
     ## PS4
     cp ./ps4/*.vpk ./
-    echo "Copying PS4 VPKs"
-    ls ./
-    echo "----------------"
     wait
     ./RSPNVPK ./englishclient_frontend.bsp.pak000_dir.vpk -d ./src -s
     wait
     # make dir ignore if exists
     echo "Making PS4 Folder"
     mkdir -p ./CUSA04013/vpk_ps4
-    wait
     mv ./*.vpk ./CUSA04013/vpk_ps4
-    wait
+
     zip -r Direct-Connect-PS4-plugin.zip ./CUSA04013/
 
     ## PC
     cp ./pc/*.vpk ./
-    echo "Copying PC VPKs"
-    ls ./
-    echo "----------------"
     wait
     ./RSPNVPK ./englishclient_frontend.bsp.pak000_dir.vpk -d ./src -s
     wait
     # make dir ignore if exists
     echo "Making PC Folder"
     mkdir -p ./Direct-Connect-PC
-    wait
     mv ./*.vpk ./Direct-Connect-PC
-    wait
+
     zip -r Direct-Connect-PC.zip ./Direct-Connect-PC/
 
 else
